790f95b0bdcc5c681482544e35538101bc9920e3
wangyong <vangyong@126.com>
2019-08-16 16:19:20 +0800

    修过

---
+++ nio/buf_test.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	std::cout << std::unitbuf;//所有输出操作立即刷新缓冲区
+	std::cout << "hello,world";
+	std::cout << std::nounitbuf;//回到正常的缓冲方式
+	system("pause");
+	return 0;
+}
