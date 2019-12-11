491fef48ffe1cd99895e434ad41b0f092cea650e
wangyong <vangyong@126.com>
2019-12-11 10:33:41 +0800

    修改

--- rocksdb/test2.cpp
+++ rocksdb/test2.cpp
@@ -12,21 +12,19 @@ std::string kDBPath = "/data/datas/rocksdb/test2";
 int main() {
   DB* db;
   Options options;
-  // Optimize RocksDB. This is the easiest way to get RocksDB to perform well
   options.IncreaseParallelism();
   options.OptimizeLevelStyleCompaction();
-  // create the DB if it's not already present
   options.create_if_missing = true;
 
-  // open DB
   Status s = DB::Open(options, kDBPath, &db);
   assert(s.ok());
 
   // Put key-value
   s = db->Put(WriteOptions(), "key1", "value");
   assert(s.ok());
+
+  // Get value
   std::string value;
-  // get value
   s = db->Get(ReadOptions(), "key1", &value);
   assert(s.ok());
   printf("value is %s \n",&value);
