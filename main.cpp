5fdf3a5e2a5b29dc34978c372819e50521a45308
root <root@localhost.localdomain>
2020-09-19 12:17:56 -0400

    修改

--- main.cpp
+++ main.cpp
@@ -6,17 +6,17 @@
 
 int main() {
     //简单打印hello world
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
 
     //输入、类型转换
     int books, months;
     double booksPerMonth;
-    std::cout << "How many books do you plan to read? ";
-    std::cin >> books;
-    std::cout << "How many months will it take you to read them? ";
-    std::cin >> months;
+    cout << "How many books do you plan to read? ";
+    cin >> books;
+    cout << "How many months will it take you to read them? ";
+    cin >> months;
     booksPerMonth = static_cast<double>(books) / months; //计算每个月阅读量
-    std::cout << "That is " << booksPerMonth << " books per month.\n";
+    cout << "That is " << booksPerMonth << " books per month.\n";
 
     //指针
     int var = 20;   // 实际变量的声明
@@ -24,20 +24,20 @@ int main() {
     ip = &var;       // 在指针变量中存储 var 的地址
     int **a;
 
-    std::cout << "Value of var variable: ";
-    std::cout << var << std::endl;
+    cout << "Value of var variable: ";
+    cout << var << endl;
 
     // 输出在指针变量中存储的地址
-    std::cout << "Address stored in ip variable: ";
-    std::cout << ip << std::endl;
+    cout << "Address stored in ip variable: ";
+    cout << ip << endl;
 
     // 访问指针中地址的值
-    std::cout << "Value of *ip variable: ";
-    std::cout << *ip << std::endl;
+    cout << "Value of *ip variable: ";
+    cout << *ip << endl;
 
     // 访问指针中地址的值
-    std::cout << "Value of * *a variable: ";
-    std::cout << &a << std::endl;
+    cout << "Value of * *a variable: ";
+    cout << &a << endl;
 
     //接口（头文件）
     Speak speak;
@@ -49,7 +49,7 @@ int main() {
     double *pvalue = NULL; // 初始化为 null 的指针
     pvalue = new double;   // 为变量请求内存
     *pvalue = 29494.99;     // 在分配的地址存储值
-    std::cout << "Value of pvalue : " << *pvalue << std::endl;
+    cout << "Value of pvalue : " << *pvalue << endl;
     delete pvalue;         // 释放内存
 
 
@@ -58,10 +58,10 @@ int main() {
         throw MyException();
     }
     catch (MyException &e) {
-        std::cout << "MyException caught" << std::endl;
-        std::cout << e.what() << std::endl;
+        cout << "MyException caught" << endl;
+        cout << e.what() << endl;
     }
-    catch (std::exception &e) {
+    catch (exception &e) {
         //其他的错误
     }
 
