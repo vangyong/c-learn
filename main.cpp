c4726d672cae5c1b3ea3b51050788820f44168fb
wangyong <vangyong@126.com>
2020-09-19 13:34:33 +0800

    xiug

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 #include "basic/exception_demo.cpp"
 #include "basic/io_demo.cpp"
 
-using namespace std;
-
 
 int main() {
     //简单打印hello world
@@ -51,7 +49,7 @@ int main() {
     double *pvalue = NULL; // 初始化为 null 的指针
     pvalue = new double;   // 为变量请求内存
     *pvalue = 29494.99;     // 在分配的地址存储值
-    cout << "Value of pvalue : " << *pvalue << endl;
+    std::cout << "Value of pvalue : " << *pvalue << std::endl;
     delete pvalue;         // 释放内存
 
 
