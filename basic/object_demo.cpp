2c51003ddaa73db0deaa5a845f1edba50042a9b3
wangyong <vangyong@126.com>
2020-09-19 11:04:11 +0800

    修改main

--- basic/object_demo.cpp
+++ basic/object_demo.cpp
@@ -12,3 +12,29 @@ public:
         return height * length * breadth;
     }
 };
+
+
+int main() {
+    //对象类
+    Box Box1;
+    Box Box2;
+    double volume = 0.0;
+
+    // box 1 详述
+    Box1.height = 5.0;
+    Box1.length = 6.0;
+    Box1.breadth = 7.0;
+
+    // box 2 详述
+    Box2.height = 10.0;
+    Box2.length = 12.0;
+    Box2.breadth = 13.0;
+
+    // box 1 的体积
+    volume = Box1.getVolume();
+    cout << "Box1 的体积：" << volume << endl;
+
+    // box 2 的体积
+    volume = Box2.getVolume();
+    cout << "Box2 的体积：" << volume << endl;
+}
--- main.cpp
+++ main.cpp
@@ -39,29 +39,6 @@ int main() {
     std::cout << "Value of * *a variable: ";
     std::cout << &a << std::endl;
 
-    //对象类
-    Box Box1;
-    Box Box2;
-    double volume = 0.0;
-
-    // box 1 详述
-    Box1.height = 5.0;
-    Box1.length = 6.0;
-    Box1.breadth = 7.0;
-
-    // box 2 详述
-    Box2.height = 10.0;
-    Box2.length = 12.0;
-    Box2.breadth = 13.0;
-
-    // box 1 的体积
-    volume = Box1.getVolume();
-    cout << "Box1 的体积：" << volume << endl;
-
-    // box 2 的体积
-    volume = Box2.getVolume();
-    cout << "Box2 的体积：" << volume << endl;
-
     //接口（头文件）
     Speak speak;
     speak.sayHello("this is my word");
